35af373e4a72a62847d872d01fb8e830f124d81d
igruk <88056536+igruk@users.noreply.github.com>
2023-05-25 23:13:45 +0300

    refactoring

--- bot.py
+++ bot.py
@@ -22,8 +22,27 @@ class UserState(StatesGroup):
     password = State()
 
 
-async def save_user_to_db(telegram_id, username, first_name, last_name, email, password, image):
+def sanitize_input(text: str) -> str:
+    """Sanitize user input by removing leading/trailing whitespace."""
+    return text.strip()
+
+
+def is_valid_email(email: str) -> str:
+    """Check if the given email is in a valid format."""
+    return re.match(r'[^@]+@[^@]+\.[^@]+', email)
+
+
+async def save_user_to_db(telegram_id, username, first_name, last_name, email, password, image) -> None:
+    """Save the user to the database."""
     password_hash = generate_password_hash(password)
+
+    if not username:
+        username = telegram_id
+    if not first_name:
+        first_name = ''
+    if not last_name:
+        last_name = ''
+
     user = User(
         telegram_id=telegram_id,
         username=username,
@@ -38,21 +57,49 @@ async def save_user_to_db(telegram_id, username, first_name, last_name, email, p
         db.session.commit()
 
 
+async def download_profile_photo(telegram_id: int) -> str:
+    """Download the profile photo of the user."""
+    profile_pictures = await dp.bot.get_user_profile_photos(telegram_id)
+    photo_file = await dp.bot.get_file(profile_pictures.photos[0][-1].file_id)
+
+    photo_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{photo_file.file_path}"
+    filename = f"user_{telegram_id}_{photo_url.split('/')[-1]}"
+    dir_path = "static/images/"
+    os.makedirs(dir_path, exist_ok=True)
+    photo_path = os.path.join(dir_path, filename)
+    image = photo_path[7:]
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(photo_url) as resp:
+            with open(photo_path, 'wb') as photo:
+                photo.write(await resp.read())
+    return image
+
+
+async def register_user(telegram_id, username, first_name, last_name, email, password) -> None:
+    """Register the user by saving their details and profile photo."""
+    image = await download_profile_photo(telegram_id)
+    await save_user_to_db(telegram_id, username, first_name, last_name, email, password, image)
+
+
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
+async def send_welcome(message: types.Message) -> None:
+    """Send a welcome message to the user."""
     await message.answer("Вітаю! Для реєстрації на сайті натисніть команду /register")
 
 
 @dp.message_handler(commands=['register'])
-async def register(message: types.Message):
+async def start_register(message: types.Message) -> None:
+    """Start the registration process."""
     await message.answer("Введіть свій email:")
     await UserState.email.set()
 
 
 @dp.message_handler(state=UserState.email)
-async def get_username(message: types.Message, state: FSMContext):
-    email = message.text
-    if re.match(r"[^@]+@[^@]+\.[^@]+", email):
+async def get_email(message: types.Message, state: FSMContext) -> None:
+    """Get the email from the user."""
+    email = sanitize_input(message.text)
+    if is_valid_email(email):
         await state.update_data(email=email)
         await message.answer("Введіть пароль:")
         await UserState.password.set()
@@ -61,40 +108,28 @@ async def get_username(message: types.Message, state: FSMContext):
 
 
 @dp.message_handler(state=UserState.password)
-async def get_password(message: types.Message, state: FSMContext):
-    await state.update_data(password=message.text)
-
+async def get_password(message: types.Message, state: FSMContext) -> None:
+    """Get the password from the user and complete the registration."""
     data = await state.get_data()
 
     telegram_id = message.from_user.id
     username = message.from_user.username
     first_name = message.from_user.first_name
     last_name = message.from_user.last_name
+    password = sanitize_input(message.text)
     email = data['email']
-    password = data['password']
-
-    profile_pictures = await dp.bot.get_user_profile_photos(message.from_user.id)
-    photo_file = await dp.bot.get_file(profile_pictures.photos[0][-1].file_id)
-    photo_url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{photo_file.file_path}"
-    filename = f"user_{message.from_user.id}_{photo_url.split('/')[-1]}"
-    dir_path = "static/images/"
-    os.makedirs(dir_path, exist_ok=True)
-    photo_path = os.path.join(dir_path, filename)
-    image = photo_path[7:]
-    async with aiohttp.ClientSession() as session:
-        async with session.get(photo_url) as resp:
-            with open(photo_path, 'wb') as photo:
-                photo.write(await resp.read())
 
     try:
-        await save_user_to_db(telegram_id, username, first_name, last_name, email, password, image)
+        await register_user(telegram_id, username, first_name, last_name, email, password)
         await message.answer("Чудово! Тепер Ви можете заходити у свій аккаунт на сайті.")
     except Exception as e:
         print(e)
         await message.answer("Ви вже зареєстровані.")
+        raise e
 
     await state.finish()
 
 
-def start_bot():
+def start_bot() -> None:
+    """Start the Telegram bot."""
     executor.start_polling(dp, skip_updates=True)
