3d659c8d042b554d3b342a0b35e7dbf3aca36c71
igruk <88056536+igruk@users.noreply.github.com>
2023-05-25 04:21:31 +0300

    refactoring

---
+++ main.py
@@ -0,0 +1,20 @@
+import threading
+import app
+import bot
+
+
+def main():
+    # Create a thread for the Flask app
+    flask_thread = threading.Thread(target=app.app.run)
+
+    # Start the Flask app thread
+    flask_thread.start()
+
+    # Start the Telegram bot
+    bot.start_bot()
+
+
+if __name__ == '__main__':
+    main()
+
+
